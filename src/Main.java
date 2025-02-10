ce54647a09145d6cc2f948c56b762b96f62cde24
Emmanuel Asamoah Botwe <noelnuel44@gmail.com>
2025-02-10 16:23:46 -0500

    updated game logic

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ import utils.UserCommand;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
+        
 
+        HashMap<String, HashSet<String>> objectSequence = Loader.loadInteractiveSequence("res/object-sequence.txt");
+        HashMap<String, UserCommand> commands = Loader.loadCommands();
 
-        HashMap<String, HashSet<String>> objectSequence =   Loader.loadInteractiveSequence("res/object-sequence.txt");
-        HashMap<String, UserCommand> commands =  Loader.loadCommands();
-
-        GameController gameController = new GameController(commands, objectSequence );
+        GameController gameController = new GameController(commands, objectSequence);
         gameController.startGame();
     }
 }
